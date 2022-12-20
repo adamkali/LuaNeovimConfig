57e6696a2b590a4db060ec186304f417fc25da13
adamkali <adamkali@outlook.com>
2022-12-20 09:03:21 -0500

    Add better vapor colors to lualine

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,12 +1,74 @@
+local colors = {
+   Background = "#0a0425",
+   Primary = {
+        shade0 = "#110066",
+        shade1 = "#1a0099",
+        shade2 = "#2b00ff",
+        shade3 = "#aa99ff",
+   },
+   Secondary = {
+        shade0 = "#660066",
+        shade1 = "#cc00cc",
+        shade2 = "#ff33ff",
+        shade3 = "#ff99ff",
+   },
+   Tertiary = {
+        shade0 = "#ccaa00",
+        shade1 = "#ffd500",
+        shade2 = "#ffe666",
+        shade3 = "#fff6cc",
+   },
+   Quaternary = {
+        shade0 = "#3b0561",
+        shade1 = "#750ac2",
+        shade2 = "#a83df5",
+        shade3 = "#d49efa",
+   },
+   Quintary = {
+        shade0 = "#008080",
+        shade1 = "#00cccc",
+        shade2 = "#33ffff",
+        shade3 = "#99ffff",
+   },
+}
+
+local vaporlush_theme = {
+  normal = {
+    a = { fg = colors.Secondary.shade1 , bg = colors.Primary.shade0 },
+    b = { fg = colors.Quaternary.shade2, bg = colors.Primary.shade2 },
+    c = { fg = colors.Quaternary.shade0, bg = colors.Primary.shade3 },
+  },
+  insert = {
+    a = { fg = colors.Primary.shade3 , bg = colors.Secondary.shade0 },
+    b = { fg = colors.Quintary.shade1, bg = colors.Secondary.shade1 },
+    c = { fg = colors.Quintary.shade0, bg = colors.Secondary.shade3 },
+  }, 
+  visual = {
+    a = { fg = colors.Quintary.shade2 , bg = colors.Tertiary.shade0 },
+    b = { fg = colors.Primary.shade2, bg = colors.Tertiary.shade1 },
+    c = { fg = colors.Primary.shade1, bg = colors.Tertiary.shade3 },
+  },
+  replace = {
+    a = { fg = colors.Tertiary.shade2 , bg = colors.Quaternary.shade0 },
+    b = { fg = colors.Tertiary.shade1, bg = colors.Quaternary.shade1 },
+    c = { fg = colors.Tertiary.shade0, bg = colors.Quaternary.shade3 },
+  },
+  command = {
+    a = { fg = colors.Quaternary.shade3 , bg = colors.Quintary.shade0 },
+    b = { fg = colors.Quaternary.shade2, bg = colors.Quintary.shade1 },
+    c = { fg = colors.Quaternary.shade0, bg = colors.Quintary.shade3 },
+  },
+}
+
+
 local function replaceVimModes()
     local mode_map = {
         n = '',
         i = '',
         c = '',
         V = '',
-        [''] = '並',
+        [''] = '',
         v = '',
-        R = '﯒',
         t = '',
         default = '',
     }
@@ -21,9 +83,9 @@ end
 require('lualine').setup {
 	options = {
         icons_enabled = true,
-        theme = 'nightfly' ,
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        theme = vaporlush_theme ,
+        component_separators = 'ﰉ',
+        section_separators = { left = '', right = '' },
         disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -38,12 +100,12 @@ require('lualine').setup {
         },
     },
 	sections = {
-		lualine_a = { replaceVimModes },
+		lualine_a = { { replaceVimModes, separator = { left = '' }, right_padding = 2 } },
 		lualine_b = {'branch', 'diff', 'diagnostics'},
 		lualine_c = {'filename', ''},
 		lualine_x = {'fileformat', 'filetype'},
 		lualine_y = { '' ,'progress'},
-		lualine_z = {'location'}
+		lualine_z = { {'location', separator = { right = '' }, left_padding = 2} }
 	},
 	inactive_sections = {
 		lualine_a = {},

a413377f5f1f88737dbdc7a265a1a995ad961420
Adam Kalinowski <adamkali@outlook.com>
2024-06-19 19:40:11 -0400

    FEAT: oil.nvim added to the config

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,30 @@
+local function oil_keybinds()
+    return {
+        {
+            "<leader>o",
+            function() require 'oil'.open_float() end,
+            desc = 'Open Oil'
+        },
+        {
+            "=o",
+            function() require 'oil'.save() end,
+            desc = 'Oil Save'
+        },
+        {
+            "<leader>O",
+            function()
+                require 'oil'.close()
+            end,
+            desc = 'Oil Close'
+        }
+    }
+end
+
+return {
+    "stevearc/oil.nvim",
+    opts = {
+        skip_confirm_for_simple_edits = true,
+        prompt_save_on_select_new_entry = false,
+    },
+    keys = oil_keybinds()
+}
